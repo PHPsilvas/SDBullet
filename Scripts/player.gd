ee459ccda2445c9b34e3d5e5595592096028de67
Paulo Henrique <phpinheirodasilva@gmail.com>
2026-01-14 20:11:09 -0300

    Adicionando novos recursos

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,16 +5,36 @@ extends CharacterBody2D
 const GRAVITY = 9.8 * 60 # Ajuste conforme sua função get_gravity()
 
 var input_direction := 0.0
-var input_jump := false # Vira true apenas no frame que o botão é apertado
+var input_jump := false
+var input_fire := false # Vira true apenas no frame que o botão é apertado
+
+var mira = 1
 
 var target_position := Vector2.ZERO
 var target_velocity := Vector2.ZERO
 
 @onready var anim := $AnimatedSprite2D
+@onready var jet_pack_particle: GPUParticles2D = %JetPackParticle
+@onready var jet_pack_bar: ProgressBar = %JetPackBar
+var bullet_path = preload("res://Bullet.tscn")
+
+const JETPACK_FORCE = 20.0
+const JETPACK_FUEL_MAX = 100
+const JETPACK_FUEL_COST = 1.0
+const JETPACK_MAXHEIGHT = 250
+
+var jetpackFuel = JETPACK_FUEL_MAX
+var jetpack_active = false
+
 
 
 func _ready():
 	target_position = global_position
+	
+	jet_pack_bar.value = jetpackFuel
+
+
+
 
 
 func _physics_process(delta):
@@ -58,17 +78,22 @@ func receive_state(pos: Vector2, vel: Vector2):
 # O Dono é o único que precisa da entrada
 func _read_input():
 	input_direction = Input.get_axis("ui_left", "ui_right")
-	input_jump = Input.is_action_just_pressed("ui_accept")
+	input_jump = Input.is_action_pressed("ui_accept")
+	input_fire = Input.is_action_just_pressed("Left_mouse")
 
 # O Dono é o único que aplica o movimento
 func _apply_movement(delta):
 	# Seu código de movimento...
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
 
-	if input_jump and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		input_jump = false # Importante! Consumir o jump para não repetir
+		
+	if input_fire:
+		fire()
+		
+	if input_jump:
+		activate_jetpack()
 
 	if input_direction != 0:
 		velocity.x = input_direction * SPEED
@@ -78,16 +103,32 @@ func _apply_movement(delta):
 	# Animações locais
 	if is_on_floor():
 		if input_direction > 0:
-			anim.flip_h = false
+			#anim.flip_h = false
+			if mira < 0:
+				self.scale.x = -1
+				mira = 1
 			anim.play("walk")
 		elif input_direction < 0:
-			anim.flip_h = true
+			#anim.flip_h = true
+			if mira > 0:
+				self.scale.x = -1
+				mira = -1
 			anim.play("walk")
 		else:
 			anim.play("idle")
 	else:
 		anim.play("jump")
-
+		if input_direction > 0:
+			#anim.flip_h = false
+			if mira < 0:
+				self.scale.x = -1
+				mira = 1
+		elif input_direction < 0:
+			#anim.flip_h = true
+			if mira > 0:
+				self.scale.x = -1
+				mira = -1
+	
 	move_and_slide()
 
 
@@ -103,3 +144,28 @@ func _apply_remote_state(delta):
 		anim.flip_h = velocity.x < 0
 	else:
 		anim.play("idle")
+
+func activate_jetpack():
+	if jetpackFuel > 0:
+		jetpack_active = true
+		velocity.y -= JETPACK_FORCE
+		jetpackFuel -= JETPACK_FUEL_COST
+		jet_pack_bar.visible = true
+		jet_pack_bar.value -= JETPACK_FUEL_COST
+		jet_pack_particle.emitting = true
+
+func _on_jet_pack_cooldown_timeout() -> void:
+	if jetpackFuel < 100 and is_on_floor():
+		jetpackFuel += JETPACK_FUEL_COST
+		jet_pack_bar.value += JETPACK_FUEL_COST
+		jetpack_active = false
+		jet_pack_particle.emitting = false
+	if jetpackFuel >= 100:
+		jet_pack_bar.visible = false
+
+func fire():
+	var bullet = bullet_path.instantiate()
+	bullet.dir = rotation
+	bullet.pos = $AnimatedSprite2D/Node2D.global_position
+	bullet.rota = global_rotation
+	get_parent().add_child(bullet)
